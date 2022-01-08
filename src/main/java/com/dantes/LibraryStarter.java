5ac30eb0fbcab92ae169fe8a61756609fe999e51
daniil_mysnik_990 <69dantes6901>
2022-01-08 14:34:19 +0600

    #1 Инициализация

---
+++ src/main/java/com/dantes/LibraryStarter.java
@@ -0,0 +1,11 @@
+package com.dantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryStarter {
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryStarter.class, args);
+    }
+}
